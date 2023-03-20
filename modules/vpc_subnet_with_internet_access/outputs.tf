83fcf019a1c21c7bed5d18e7ef6963c2bba01349
GoodmanBen <goodmanben@gmail.com>
2023-03-20 12:19:11 -0400

    fix: subnet output is id not arn

--- modules/vpc_subnet_with_internet_access/outputs.tf
+++ modules/vpc_subnet_with_internet_access/outputs.tf
@@ -1,9 +1,9 @@
 output "security_group" {
   description = "Security group for ECS fargate task networking."
-  value       = aws_security_group.ecs_fargate_sg.arn
+  value       = aws_security_group.ecs_fargate_sg.id
 }
 
 output "subnet" {
   description = "Subnet within which the ECS Fargate Task is placed."
-  value       = aws_subnet.public_subnet.arn
+  value       = aws_subnet.public_subnet.id
 }
